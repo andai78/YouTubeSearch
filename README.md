7a1a27779356a97cfdecae94528c037b86957b9d
andai78 <tyrel92@hotmail.com>
2018-01-20 17:12:02 +0100

    creation du serveur express et du repertoire dist

---
+++ README.md
@@ -0,0 +1,5 @@
+# YTSearch
+
+Web app permettant d'effectuer des recherches sur You tube. Techno : Angular 4, Node, Express, Mongo.
+Basé sur le tutoriel de Samir Medjoub.
+
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// creation d'un middleware pour acceder au repertoire dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', (req, res) => {
+    res.send('app works')
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'dist/index.html'));
+});
+
+const port = process.env.port || 3000;
+app.set('port', port);
+
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`)
+})
